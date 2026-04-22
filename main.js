7f54b2d6fd5202726118a6bcdcc0b7a0f1f6e604
Carina Schädlich <carina.schaedlich@gmail.com>
2026-04-22 12:04:56 +0200

    added styling for type and id, added favourite/catch button

--- main.js
+++ main.js
@@ -7,27 +7,69 @@ async function loadPokemon(){
         try {
             const res = await fetch(`${path}/${count}`)
             const data = await res.json()
+            const type = data.types[0].type.name
+            const name = data.name
+            const image = data.sprites.other.home.front_default
+            let typeColor = "rounded-3xl py-1 px-3 text-xs text-white uppercase"
+            if (type =="grass"){
+                typeColor += " bg-green-400"            
+            }else if(type=="fire"){
+                typeColor += " bg-red-500"
+            }else if(type =="water"){
+                typeColor +=" bg-blue-400"
+            }else if(type =="bug"){
+                typeColor +=" bg-[#acba5f]"
+            }else if(type =="poison"){
+                typeColor +=" bg-violet-400"
+            }else if(type =="psychic"){
+                typeColor +=" bg-pink-400"
+            }else if(type =="ghost"){
+                typeColor +=" bg-[#864491]"
+            }else if(type =="ground"){
+                typeColor +=" bg-orange-800"
+            }else if(type =="fairy"){
+                typeColor +=" bg-fuchsia-400"
+            }else if(type =="fighting"){
+                typeColor +=" bg-orange-400"
+            }else if(type =="rock"){
+                typeColor +=" bg-[#c7cca9]"
+            }else if(type =="electric"){
+                typeColor +=" bg-yellow-300"
+            }else if(type =="ice"){
+                typeColor +=" bg-blue-200"
+            }else if(type =="dragon"){
+                typeColor +=" bg-indigo-500"
+            }else{
+                typeColor +=" bg-gray-300"
+            }
+
+            let pokeID = data.id
+            if(pokeID <10){
+                pokeID = `00${pokeID}`
+            }else if(pokeID >=10 && pokeID<100){
+                pokeID = `0${pokeID}`
+            }else{
+                pokeID = `${pokeID}`
+            }
         
                 let newHTML = `
-                <div class="pokemon-card rounded-md bg-gray-100 flex flex-col justify-center items-center shadow-md pt-2">
-                    <p id = "poke-id" class = "bg-black text-white p-1 rounded-2xl text-xs">#00${data.id}</p>
-                    <img class = "w-full h-full object-contain pb-8" src=${data.sprites.other.home.front_default}>
-                    <div class = "p-2 bg-white min-w-full flex flex-col justify-center items-center rounded-md">
-                        <p id="poke-name" class="font-bold text-lg">${data.name}</p>
-                        <p id = "poke-type" class="">${data.types[0].type.name}</p>
+                <div class="pokemon-card rounded-md bg-gray-100 flex flex-col justify-center items-center shadow-md">
+                    <div class="flex flex-col justify-center items-center pt-2">
+                        <div class="flex justify-between items-center w-full px-2">
+                            <p class = "poke-id bg-black text-white p-1 rounded-2xl text-xs">#${pokeID}</p>
+                            <button class="text-2xl bg-white px-2 rounded-full">♡</button>
+                        </div>
+                        <img class = "w-full h-full object-contain pb-8" src=${image}>
+                    </div>
+                    <div class = "p-2 bg-white min-w-full flex flex-col justify-center items-start rounded-md">
+                        <p class="poke-name font-bold text-lg capitalize pb-2">${name}</p>
+                        <p class="poke-type ${typeColor}">${type}</p>
                     </div>                                
                 `
                 container.innerHTML += newHTML
                 console.log(data.types[0].type.name);
                 
-                // const type = document.getElementById("poke-type")
-                // if(data.types[0].type.name=="grass"){
-                //     type.classList.add("bg-green-300")             
-                // }else if(data.types[0].type.name=="fire"){
-                //     type.classList.add("bg-red-300")
-                // }else{
-                //     type.classList.add("bg-blue-300")
-                // }
+               
                 count++
             
         } catch(err) {console.error(err)}
